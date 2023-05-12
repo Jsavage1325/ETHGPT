9c704e081dc4a7d215f814b7107d3161cb5e68fb
hpink97 <64581037+hpink97@users.noreply.github.com>
2023-05-12 16:09:16 +0000

    add AirStackDocSearch

--- helpers/retrieve_docs.py
+++ helpers/retrieve_docs.py
@@ -26,3 +26,27 @@ class LangchainDocSearch(BaseTool):
     async def _arun(self, query: str, run_manager: Optional[AsyncCallbackManagerForToolRun] = None) -> str:
         """Use the tool asynchronously."""
         raise NotImplementedError("langchain_doc_search does not support async")
+    
+class AirStackDocSearch(BaseTool):
+    name = "airstack_doc_search"
+    description = "AirStack expert who will tell you how to do specific things using airstack."
+
+    def load_vector_store(self):
+        """
+        Loads the airstack vector store from pickle into a local file
+        """
+        global vector_store
+        with open("airstack_vectorstore.pkl", "rb") as f:
+            vector_store = pickle.load(f)
+
+    def _run(self, query: str) -> str:
+        """
+        Search the docs and return the most relevant pages content which we will use to feed to the model
+        """
+        res = vector_store.similarity_search(query)
+        if res:
+            return res[0].page_content, res[0].metadata['source']
+    
+    async def _arun(self, query: str, run_manager: Optional[AsyncCallbackManagerForToolRun] = None) -> str:
+        """Use the tool asynchronously."""
+        raise NotImplementedError("airstack_doc_search does not support async")
