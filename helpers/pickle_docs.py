55e0ce41a824622353876cca92955ee160275912
hpink97 <64581037+hpink97@users.noreply.github.com>
2023-05-12 16:08:03 +0000

    add compatibility for gitbooks in doc reader

--- helpers/pickle_docs.py
+++ helpers/pickle_docs.py
@@ -1,9 +1,12 @@
 import pickle
-from langchain.document_loaders import ReadTheDocsLoader
+import os
+from langchain.document_loaders import ReadTheDocsLoader, GitbookLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 import subprocess
+
+os.environ['OPENAI_API_KEY'] = "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"
 
 def download_docs(docs_url: str, output_name: str):
     """
@@ -17,17 +20,18 @@ def download_docs(docs_url: str, output_name: str):
     command = f"wget --recursive --no-parent --random-wait --limit-rate=200k --wait=1 -e robots=off --no-check-certificate -U \"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3\" -P {output_name} {docs_url}"
     subprocess.run(command, shell=True)
 
-def ingest_docs(docs_loc: str="rtdocs", docs_name: str='langchain'):
+def ingest_docs(docs_loc: str="rtdocs", docs_name: str='langchain', gitbook=False):
     """Get documents from web pages."""
-    loader = ReadTheDocsLoader(docs_loc, features='html.parser')
-    print(loader)
+    if gitbook:
+        loader = GitbookLoader(docs_loc, load_all_paths=True)
+    else:
+        loader = ReadTheDocsLoader(docs_loc, features='html.parser')
     raw_documents = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,
-        chunk_overlap=200,
-    )
+    text_splitter = RecursiveCharacterTextSplitter()
+    print('raw docs')
     print(raw_documents)
     documents = text_splitter.split_documents(raw_documents)
+    print('docs')
     print(documents)
     embeddings = OpenAIEmbeddings()
 
@@ -39,8 +43,9 @@ def ingest_docs(docs_loc: str="rtdocs", docs_name: str='langchain'):
     print(docs[0].page_content)
 
     # Save vectorstore
-    with open("vectorstore.pkl", "wb") as f:
+    with open(f"{docs_name}_vectorstore.pkl", "wb") as f:
         pickle.dump(vectorstore, f)
 
-download_docs('https://docs.airstack.xyz', 'airstack')
+#download_docs('https://docs.airstack.xyz', 'airstack')
 # download_docs('https://python.langchain.com/', 'langchain')
+#ingest_docs(docs_loc='https://docs.airstack.xyz',docs_name='airstack',gitbook=True)
