91a4609768707a72536cfb4d823d241b65de057c
spink-eth <spink@metaverseholdings.tech>
2023-05-13 15:52:27 +0100

    updated the ui

--- eth-agent/coolbackhandaler.py
+++ eth-agent/coolbackhandaler.py
@@ -1,6 +1,7 @@
 from typing import Optional, Dict, Any
 
 from dotenv import load_dotenv
+from ai_helper import AIHelper
 from tools.code.PythonCodeWriter import PythonCodeWriter
 from experts.airstack_expert import AirstackContextProvider
 from experts.langchain_expert import LangchainContextProvider
@@ -23,24 +24,22 @@ class StreamlitCallbackHandler(BaseCallbackHandler):
         """Initialize callback handler."""
         self.color = color
 
-    # Method definitions remain the same, replace all print statements with Streamlit's st.write()
-
     def on_chain_start(
         self, serialized: Dict[str, Any], inputs: Dict[str, Any], **kwargs: Any
     ) -> None:
         """Print out that we are entering a chain."""
         class_name = serialized["name"]
-        st.write(f"\n\n> Entering new {class_name} chain...")
+        st.markdown(f"> Entering new **{class_name}** chain...")
 
     def on_chain_end(self, outputs: Dict[str, Any], **kwargs: Any) -> None:
         """Print out that we finished a chain."""
-        st.write("\n> Finished chain.")
+        st.markdown("> **Finished chain.**")
 
     def on_agent_action(
         self, action: AgentAction, color: Optional[str] = None, **kwargs: Any
     ) -> Any:
         """Run on agent action."""
-        st.write(action.log)
+        st.markdown(action.log)
 
     def on_tool_end(
         self,
@@ -52,10 +51,10 @@ class StreamlitCallbackHandler(BaseCallbackHandler):
     ) -> None:
         """If not the final action, print out observation."""
         if observation_prefix is not None:
-            st.write(f"\n{observation_prefix}")
-        st.write(output)
+            st.markdown(f"\n{observation_prefix}")
+        st.markdown(output)
         if llm_prefix is not None:
-            st.write(f"\n{llm_prefix}")
+            st.markdown(f"\n{llm_prefix}")
 
     def on_text(
         self,
@@ -65,49 +64,17 @@ class StreamlitCallbackHandler(BaseCallbackHandler):
         **kwargs: Any,
     ) -> None:
         """Run when agent ends."""
-        st.write(text)
+        st.markdown(text)
 
     def on_agent_finish(
         self, finish: AgentFinish, color: Optional[str] = None, **kwargs: Any
     ) -> None:
         """Run on agent end."""
-        st.write(finish.log)
+        st.markdown(finish.log)
 
 
 # load env
 load_dotenv()
-
-
-class AIHelper:
-    def __init__(self):
-        self.llm = OpenAI(temperature=0.0)
-        self.search = SerpAPIWrapper()
-
-        self.tools = [
-            PythonCodeWriter(),
-            EthSend(),
-            GetEthBalance(),
-            LangchainContextProvider(),
-            AirstackContextProvider(),
-            AaveContextProvider(),
-        ]
-
-        self.agent = initialize_agent(
-            self.tools,
-            self.llm,
-            agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-            streaming=True,
-            callbacks=[StreamlitCallbackHandler()],
-        )
-
-    def run_query(self, query):
-        self.agent.run(
-            {
-                "input": query,
-                "chat_history": [],
-            }
-        )
-
 
 if __name__ == "__main__":
     st.set_page_config(page_title="LangChain Demo", page_icon=":robot:")
@@ -128,10 +95,12 @@ if __name__ == "__main__":
     user_input = get_text()
 
     if user_input:
-        output = helper.run_query(user_input)
-
-        st.session_state.past.append(user_input)
-        st.session_state.generated.append(output)
+        helper.agent.run(
+            {
+                "input": user_input,
+                "chat_history": [],
+            }
+        )
 
     if st.session_state["generated"]:
         for i in range(len(st.session_state["generated"]) - 1, -1, -1):
